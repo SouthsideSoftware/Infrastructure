8ebed9008316ab42b3193809b915d2f0af830f1a
Tom Cabanski <tom@cabanski.com>
2011-10-13 10:00:49 -0500

    Add string helper for truncate and some tests.

---
+++ src/Infrastructure.Core/Helpers/StringHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infrastructure.Core.CodeContracts;
+
+namespace Infrastructure.Core.Helpers {
+    /// <summary>
+    /// Extensions for string.
+    /// </summary>
+    public static class StringHelper {
+        /// <summary>
+        /// Trim the string to a max length.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="maxLength"></param>
+        /// <returns></returns>
+        public static string TrimToMaxLength(this string source, int maxLength) {
+            ParameterCheck.StringRequiredAndNotWhitespace(source, "source");
+            ParameterCheck.IntParameterGreaterThanZero(maxLength, "maxLength");
+
+            return (source != null && source.Length > maxLength) ? source.Substring(0, maxLength) : source;
+        }
+    }
+}
---
+++ src/Tests/Infrastructure.Core.Tests/Unit/Helpers/StringHelper/TrimToMaxLengthTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FluentAssertions;
+using Infrastructure.Core.Helpers;
+
+namespace Infrastructure.Core.Tests.Unit.Helpers.StringHelper {
+    [TestFixture]
+    public class TrimToMaxLengthTests {
+        [Test]
+        public void TrimToMaxLength_StringLessThanMax_ReturnsString() {
+            var s = "this is unchanged";
+            s.TrimToMaxLength(100).Should().Be("this is unchanged");
+        }
+
+        [Test]
+        public void TrimToMaxLength_StringExactlyMaxLength_ReturnsString() {
+            var s = "length 8";
+            s.TrimToMaxLength(8).Should().Be("length 8");
+        }
+
+        [Test]
+        public void TrimToMaxLength_StringOneMoreThanMaxLength_ReturnsStringWithmaxLength() {
+            var s = "length 8";
+            s.TrimToMaxLength(7).Should().Be("length ");
+        }
+
+        [Test]
+        public void TrimToMaxLength_StringMuchMoreThanMaxLength_ReturnsStringWithmaxLength() {
+            var s = "This is a pretty long string";
+            s.TrimToMaxLength(1).Should().Be("T");
+        }
+    }
+}
