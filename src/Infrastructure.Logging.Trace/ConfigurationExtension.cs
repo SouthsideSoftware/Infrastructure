c422d9ec234d370726d3472004709e775a6b09fe
Tom Cabanski <tom@cabanski.com>
2011-10-17 06:56:39 -0500

    Add extension to configure trace.

---
+++ src/Infrastructure.Logging.Trace/ConfigurationExtension.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Core;
+using Infrastructure.Core.CodeContracts;
+
+namespace Infrastructure.Logging.Trace
+{
+    /// <summary>
+    /// Extensions for working with configuration.
+    /// </summary>
+    public static class ConfigurationExtension
+    {
+        /// <summary>
+        /// Log using Trace
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static Configuration LogWithTrace(this Configuration configuration)
+        {
+            ParameterCheck.ParameterRequired(configuration, "configuration");
+
+            configuration.LogWith(new TraceLogProvider());
+            return configuration;
+        }
+    }
+}
